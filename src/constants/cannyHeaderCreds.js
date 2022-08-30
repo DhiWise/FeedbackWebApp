8fbc10017f92dc5797945115a141d49c308a9961
AshutoshDaveDhiWise <ashutosh.dave@dhiwise.com>
2022-08-30 15:05:44 +0530

    removed apikey from application

--- src/constants/cannyHeaderCreds.js
+++ src/constants/cannyHeaderCreds.js
@@ -1,3 +1,3 @@
-export const apikey = "1b3b36d5-20a5-0ac4-ff20-12ea8b656df0";
-export const boardid = "62de426363bf6644bc72820b";
-export const authorid = "618cc142a09c4f6988785b5b";
+export const apikey = "[your canny apiKey]";
+export const boardid = "[your canny boardId]";
+export const authorid = "[your canny authorId]";
